606982ba6e0a4785fcef3829857fab5abea1a122
gaagnec <gaagnec@gmail.com>
2022-10-17 17:09:22 +0300

    feat: task get email-domain done

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,11 +10,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let pos = email.lastIndexOf('@');
+  let result = email.slice(pos + 1);
+  return result;
 }
 
 module.exports = {
-  getEmailDomain
+  getEmailDomain,
 };
--- src/temp.js
+++ src/temp.js
@@ -1,45 +1,9 @@
-// let arr = [
-//   1,
-//   [8, [[]]],
-//   2,
-//   3,
-//   [8, [[[[[[[[[[[[[]]]]]]]]]]]]]],
-//   4,
-//   5,
-//   ['6575', ['adas', ['dfg']]],
-// ];
+let arr = '.unusual.com@usual.com';
 
-// function calculateDepth(arr) {
-//   let count = 0;
-//   let result = 1;
-//   for (let key in arr) {
-//     console.log('in: ', arr[key], typeof arr[key]);
-//     if (Array.isArray(arr[key])) {
-//       count = calculateDepth(arr[key]);
-//       console.log(arr[key]);
-//     }
-//     count += 1;
-//     if (count > result) result = count;
-//   }
-
-//   count = 0;
-//   return result;
-// }
-
-// console.log(calculateDepth(arr));
-let arr = 'aabbccde';
-score(arr);
-
-function score(dice) {
-  let count = 1;
-  let result = '';
-  for (let i = 0; i < dice.length; i++) {
-    if (dice[i] == dice[i + 1]) {
-      count++;
-    } else {
-      result += count + dice[i];
-      count = 1;
-    }
-  }
+function getEmailDomain(email) {
+  let pos = email.indexOf('@');
+  let result = email.slice(pos + 1);
   return result;
 }
+
+console.log(getEmailDomain(arr));
